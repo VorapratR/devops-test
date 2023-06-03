3fda303637b0991151f8865cc5a8867ad12327f2
Voraprat Rattana <vorapratrattana@gmail.com>
2023-06-03 13:43:41 +0700

    [ETC]

--- k8s/1-deployment.sh
+++ k8s/1-deployment.sh
@@ -1,3 +1,3 @@
 kubectl create deployment -n lynx-dev --image=vvoraprat/devops-lynx-dev:latest
 kubectl create deployment -n lynx-uat --image=vvoraprat/devops-lynx-uat:latest
-kubectl create deployment -n lynx-prod --image=vvoraprat/devops-lynx-prod:latest
+kubectl create deployment -n lynx-prod --image=vvoraprat/devops-lynx-prod:v1.0.0
