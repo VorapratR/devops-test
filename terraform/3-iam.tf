06517fc63dcdf9cd825ee1bf0fb43383970c0617
Voraprat Rattana <vorapratrattana@gmail.com>
2023-06-11 14:02:17 +0700

    [Add] iam eks-allow-access

---
+++ terraform/3-iam.tf
@@ -0,0 +1,21 @@
+
+module "allow_eks_access_iam_policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.3.1"
+
+  name          = "eks-allow-access"
+  create_policy = true
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "eks:DescribeCluster",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
